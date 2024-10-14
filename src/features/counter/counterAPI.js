671ef9d0000e39ec136ca029ef850ac37f3d99a4
Arsalan <arsalan.online24@gmail.com>
2024-10-14 21:38:05 +0500

    changes

--- src/features/counter/counterAPI.js
+++ src/features/counter/counterAPI.js
@@ -1,6 +1,10 @@
 // A mock function to mimic making an async request for data
 export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
+  return new Promise((resolve) =>{
+   const response =  fetch('http://localhost:8080')
+  const data =  response.json()
+  resolve({data})
+  }
   );
+
 }
