d8a5d676121f25976977aeaa20947752c05bd713
Arsalan <arsalan.online24@gmail.com>
2024-10-17 19:51:37 +0500

    cleaning code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Counter } from "./features/counter/Counter";
+
 
 
 function App() {
   return (
-    <div className="lg:flex lg:items-center lg:justify-center ">
-      <Counter />
+    <div >
+    
     </div>
   );
 }
