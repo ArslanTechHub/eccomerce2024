8a04df58e2e01aa27cfc9a557a959e4433ba1012
Arsalan <arsalan.online24@gmail.com>
2024-10-22 21:42:44 +0500

    basic routing done

--- src/features/auth/components/Signup.js
+++ src/features/auth/components/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { increment, incrementAsync, selectCount } from "../authSlice";
+import { Link } from "react-router-dom";
 
 export default function Signup() {
   const count = useSelector(selectCount);
@@ -102,12 +103,12 @@ export default function Signup() {
 
           <p className="mt-10 text-sm text-center text-gray-500">
             Already a Member?{" "}
-            <a
-              href="#"
+            <Link to = "/login"
+             
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
               Log In
-            </a>
+            </Link>
           </p>
         </div>
       </div>
