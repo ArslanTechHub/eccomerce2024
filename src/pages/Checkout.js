ac5ec148efb354e5aae899b85b0b6429f1d7f772
Arsalan <arsalan.online24@gmail.com>
2024-10-27 20:53:20 +0500

    completed checkout page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -31,22 +31,22 @@ const products = [
 ];
 
 const addresses = [
-    {
-        name: 'John Wick',
-        street: '11th Main',
-        city: 'Delhi',
-        pinCode: 110001,
-        state: 'Delhi',
-        phone: 12312321331
-    },
-    {
-        name: 'John Doe',
-        street: '15th Main',
-        city: 'Bangalore',
-        pinCode: 560034,
-        state: 'Karnataka',
-        phone: 1231231231
-    }
+  {
+    name: "John Wick",
+    street: "11th Main",
+    city: "Delhi",
+    pinCode: 110001,
+    state: "Delhi",
+    phone: 12312321331,
+  },
+  {
+    name: "John Doe",
+    street: "15th Main",
+    city: "Bangalore",
+    pinCode: 560034,
+    state: "Karnataka",
+    phone: 1231231231,
+  },
 ];
 
 const Checkout = () => {
@@ -57,7 +57,7 @@ const Checkout = () => {
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5">
           <div className="lg:col-span-3">
-            <form className="px-6 py-6 mt-12 bg-white">
+            <form className="px-6 py-6 mt-12 mb-12 bg-white">
               <div className="space-y-12">
                 <div className="pb-12 border-b border-gray-900/10">
                   <h2 className="text-base font-semibold leading-7 text-gray-900">
@@ -222,36 +222,44 @@ const Checkout = () => {
                     Address
                   </h2>
                   <p className="mt-1 text-sm leading-6 text-gray-600">
-                   Choose from Existing addresses
+                    Choose from Existing addresses
                   </p>
 
-
-
                   <ul role="list" className="mt-4 divide-y divide-gray-100">
-        {addresses.map((address, index) => (
-          <li
-            key={index}
-            className="flex justify-between px-4 py-4 rounded-lg gap-x-6 hover:bg-gray-100"
-          >
-            <div className="flex items-start gap-x-4">
-              <input
-                type="radio"
-                name="address"
-                className="w-4 h-4 text-indigo-600 border-gray-300"
-              />
-              <div className="flex-auto min-w-0">
-                <p className="text-sm font-semibold text-gray-900">{address.name}</p>
-                <p className="mt-1 text-sm text-gray-500">{address.street}</p>
-                <p className="mt-1 text-sm text-gray-500">{address.pinCode}</p>
-              </div>
-            </div>
-            <div className="flex flex-col items-end">
-              <p className="text-sm font-medium text-gray-900">Phone: {address.phone}</p>
-              <p className="text-sm text-gray-500">{address.city}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+                    {addresses.map((address, index) => (
+                      <li
+                        key={index}
+                        className="flex justify-between px-4 py-4 rounded-lg gap-x-6 hover:bg-gray-100"
+                      >
+                        <div className="flex items-start gap-x-4">
+                          <input
+                            type="radio"
+                            name="address"
+                            className="w-4 h-4 text-indigo-600 border-gray-300"
+                          />
+                          <div className="flex-auto min-w-0">
+                            <p className="text-sm font-semibold text-gray-900">
+                              {address.name}
+                            </p>
+                            <p className="mt-1 text-sm text-gray-500">
+                              {address.street}
+                            </p>
+                            <p className="mt-1 text-sm text-gray-500">
+                              {address.pinCode}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="flex flex-col items-end">
+                          <p className="text-sm font-medium text-gray-900">
+                            Phone: {address.phone}
+                          </p>
+                          <p className="text-sm text-gray-500">
+                            {address.city}
+                          </p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
 
                   <div className="mt-10 space-y-10">
                     <fieldset>
@@ -312,16 +320,14 @@ const Checkout = () => {
               </div>
             </form>
           </div>
-{/* 
 
           <div className="lg:col-span-2">
             <div className="min-h-screen py-8 bg-gray-100">
               <div className="max-w-3xl px-4 mx-auto sm:px-6 lg:px-8">
                 <div className="mt-4 bg-white rounded-lg shadow">
-                <h1 className="flex items-start justify-start px-5 py-5 ml-2 text-3xl font-bold text-gray-900">
-                  
-                  Cart
-                </h1>
+                  <h1 className="flex items-start justify-start px-5 py-5 ml-2 text-3xl font-bold text-gray-900">
+                    Cart
+                  </h1>
                   <div className="px-4 py-6 sm:px-6">
                     <div className="flow-root">
                       <ul
@@ -337,7 +343,6 @@ const Checkout = () => {
                                 className="object-cover object-center w-full h-full"
                               />
                             </div>
-
                             <div className="flex flex-col flex-1 ml-4">
                               <div className="flex justify-between text-base font-medium text-gray-900">
                                 <h3>
@@ -348,18 +353,18 @@ const Checkout = () => {
                               <p className="mt-1 text-sm text-gray-500">
                                 {product.color}
                               </p>
-                              <div className="flex items-end justify-between flex-1 text-sm">
-                                <div className="text-gray-500">
+                              <div className="flex items-center justify-between flex-1 text-sm">
+                                <div className="flex items-center space-x-2 text-gray-500">
                                   <label
                                     htmlFor="quantity"
-                                    className="block text-sm font-medium leading-6 text-gray-900"
+                                    className="text-sm font-medium leading-6 text-gray-900"
                                   >
                                     Qty
                                   </label>
                                   <select
                                     id="quantity"
                                     name="quantity"
-                                    className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                                    className="border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
                                   >
                                     <option value="1">1</option>
                                     <option value="2">2</option>
@@ -390,13 +395,13 @@ const Checkout = () => {
                   <div className="px-4 py-6 border-t border-gray-200 sm:px-6">
                     <div className="flex justify-between text-base font-medium text-gray-900">
                       <p>Subtotal</p>
-                      <p>$122.00</p>
+                      <p>$262.00</p>
                     </div>
                     <p className="mt-0.5 text-sm text-gray-500">
                       Shipping and taxes calculated at checkout.
                     </p>
                     <div className="mt-6">
-                      <button className="flex items-center justify-center w-full px-6 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700">
+                      <button className="flex items-center justify-center w-full px-6 py-3 text-base font-medium text-white bg-[#4F46E5] border border-transparent rounded-md shadow-sm hover:bg-[#4F46E5]">
                         Checkout
                       </button>
                     </div>
@@ -418,99 +423,7 @@ const Checkout = () => {
                 </div>
               </div>
             </div>
-          </div> */}
-
-
-
-<div className="lg:col-span-2">
-    <div className="min-h-screen py-8 bg-gray-100">
-        <div className="max-w-3xl px-4 mx-auto sm:px-6 lg:px-8">
-            <div className="mt-4 bg-white rounded-lg shadow">
-                <h1 className="flex items-start justify-start px-5 py-5 ml-2 text-3xl font-bold text-gray-900">
-                    Cart
-                </h1>
-                <div className="px-4 py-6 sm:px-6">
-                    <div className="flow-root">
-                        <ul role="list" className="-my-6 divide-y divide-gray-200">
-                            {cartProducts.map((product) => (
-                                <li key={product.id} className="flex py-6">
-                                    <div className="flex-shrink-0 w-24 h-24 overflow-hidden border border-gray-200 rounded-md">
-                                        <img src={product.imageSrc} alt={product.imageAlt} className="object-cover object-center w-full h-full" />
-                                    </div>
-                                    <div className="flex flex-col flex-1 ml-4">
-                                        <div className="flex justify-between text-base font-medium text-gray-900">
-                                            <h3>
-                                                <a href={product.href}>{product.name}</a>
-                                            </h3>
-                                            <p className="ml-4">{product.price}</p>
-                                        </div>
-                                        <p className="mt-1 text-sm text-gray-500">
-                                            {product.color}
-                                        </p>
-                                        <div className="flex items-center justify-between flex-1 text-sm">
-                                            <div className="flex items-center space-x-2 text-gray-500">
-                                                <label htmlFor="quantity" className="text-sm font-medium leading-6 text-gray-900">
-                                                    Qty
-                                                </label>
-                                                <select
-                                                    id="quantity"
-                                                    name="quantity"
-                                                    className="border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                                                >
-                                                    <option value="1">1</option>
-                                                    <option value="2">2</option>
-                                                    <option value="3">3</option>
-                                                    <option value="4">4</option>
-                                                </select>
-                                            </div>
-                                            <div className="flex">
-                                                <button type="button" className="inline-flex items-center px-2 py-1 text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                                                    <TrashIcon className="w-5 h-5 mr-1 text-red-500" aria-hidden="true" />
-                                                    Remove
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
-
-                <div className="px-4 py-6 border-t border-gray-200 sm:px-6">
-                    <div className="flex justify-between text-base font-medium text-gray-900">
-                        <p>Subtotal</p>
-                        <p>$262.00</p>
-                    </div>
-                    <p className="mt-0.5 text-sm text-gray-500">
-                        Shipping and taxes calculated at checkout.
-                    </p>
-                    <div className="mt-6">
-                        <button className="flex items-center justify-center w-full px-6 py-3 text-base font-medium text-white bg-[#4F46E5] border border-transparent rounded-md shadow-sm hover:bg-[#4F46E5]">
-                            Checkout
-                        </button>
-                    </div>
-                    <div className="flex justify-center mt-6 text-sm text-center text-gray-500">
-                        <p>
-                            or{" "}
-                            <Link to="/">
-                                <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                    Continue Shopping
-                                    <span aria-hidden="true"> &rarr;</span>
-                                </button>
-                            </Link>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-
-          
+          </div>
         </div>
       </div>
     </>
